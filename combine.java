f467b39d7fc740134c0081255f4aee2346641cd6
ayushvarshney009 <94174105+ayushvarshney009@users.noreply.github.com>
2021-11-13 00:47:37 +0530

    Create combine.java

---
+++ combine.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class combine {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+        System.out.println(a + " University " + b);
+
+        String c = scanner.nextLine();
+        String d = scanner.nextLine();
+        System.out.println(c + " Technologies " + d);
+    }
+}
