bf993072fa66cfa10f97344349f0116668e1e054
ayushvarshney009 <94174105+ayushvarshney009@users.noreply.github.com>
2021-11-13 08:03:17 +0530

    Create welcome.java

---
+++ welcome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class welcome {
+
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+
+        System.out.println("Welcome " + name);
+    }
+}
